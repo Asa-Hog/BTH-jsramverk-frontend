455c27e573a08aa3b2f70e8f6023ccf3b5b6277c
Åsa <asa_hoglund@hotmail.com>
2022-11-01 14:46:28 +0100

    Kmom04 sockets klart

--- texteditor/src/components/Editor.js
+++ texteditor/src/components/Editor.js
@@ -1,60 +1,111 @@
 import React, { useState, useEffect } from 'react';
-// import ReactHtmlParser from 'react-html-parser';
 import { TrixEditor } from "react-trix";
-// import "trix";
 import "trix/dist/trix.css";
-// import saveForm from './saveForm';
 import { io } from "socket.io-client";
-
+// import ReactHtmlParser from 'react-html-parser';
+// import "trix";
 import docsModel from '../models/docsModel';
 
+let sendToSocket = true;
 
 const Editor = () => {
-    // let socket;
+    let updateCurrentDocOnChange;
     let [data, setData] = useState('');
     const [name, setName] = useState('');
     const [docs, setDocs] = useState([]);
     const [currentDoc, setCurrentDoc] = useState({});
+    const [selectedDoc, setSelectedDoc] = useState({}); // id på valt objekt
     const [socket, setSocket] = useState(null);
 
-    // Hämta alla dokument
+    // // Hämta alla dokument
     useEffect(() => {
         (async () => {
             const allDocs = await docsModel.getAllDocs();
             setDocs(allDocs);
         })();
-    }, [currentDoc]);
+    }, [currentDoc]); //Laddas om när currentDoc ändras (Om man vill hämta dok från andra browsers?)
 
-    // Skapa en socket mellan frontend och backend
+
+    // Skapa en socket mellan frontend och backend - om selectedDoc ändras - till nya dokumentet
     useEffect(() => {
         setSocket(io("http://localhost:1337")); // Ändra från hårdkodat
+        if (socket) {
+            console.log("create room", selectedDoc);
+            socket.emit("create", selectedDoc);
+        }
 
         return () => {
             if (socket) {
                 socket.disconnect();
             }
         }
-        //eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [currentDoc]); // (selectedDoc?)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selectedDoc]);
 
 
-    // Varje gång det skapas/ ändras i en socket, skriv ut i consolen
+    // Skriver ut innehållet i editorn när data ändras
+    useEffect(() => {
+        setEditorContent(data, false);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [data]);
+
+
+    // Varje gång det ändras i en socket
     useEffect(() => {
         if (socket) {
-            socket.on("doc", function(data) {
-                console.log("doc", data);
-                setData(data.html);
-                setName(data.name);
-                setEditorContent(data);
+            socket.on('changedText', (doc) => {
+                console.log("3 received in clients", doc);
+                sendToSocket = false;
+                setEditorContent(doc.html, false);
             });
         }
-    });//[socket]??
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [socket]);
 
-    // if (socket) {
-    //     socket.on("doc", (data) => {
-    //         setEditorData(data.html, false);
-    //     });
-    // }
+    useEffect(() => {
+        if (socket && sendToSocket) {
+            console.log("1 emit from client", currentDoc);
+            socket.emit("changedText", currentDoc);
+        }
+        sendToSocket = true;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentDoc]); // Gör en emit om INNEHÅLLET i currentDoc ändras
+
+
+    function setEditorContent(content, triggerChange) {
+        let element = document.querySelector("trix-editor");
+
+        updateCurrentDocOnChange = triggerChange;
+        element.value = "";
+        element.editor.setSelectedRange([0, 0]); //
+        updateCurrentDocOnChange = triggerChange;
+        element.editor.insertHTML(content);
+    };
+
+    function handleChange (html, text) { // html = event
+        if (updateCurrentDocOnChange) {
+            // Skapa nytt obj (copy, som är kopia av currentDoc)
+            const copy = Object.assign({}, currentDoc);
+            copy.html = html; // Nya objeketet har nya texten sparad i html
+            setCurrentDoc(copy); // Skriv över currentDoc
+        }
+
+        updateCurrentDocOnChange = true;
+    };
+
+    let handleSelectedDoc = () => {
+        // Hämta värde ur selectlista
+        let docId = document.getElementById("selectDoc").value; // 0, 1, 2
+        let doc = docs[docId]; // {"_id": 6140s3f01sd, "title": Saga, "html": Det var en gång}
+
+        setSelectedDoc(doc["_id"]); // 6140s3f01sd - Ett id  1 currentDoc och selectedDoc sätts
+        setCurrentDoc(doc); // Ett objekt
+        setData(doc.html);
+     };
+
+    const resetDb = async () => {
+        await docsModel.reset();
+    };
 
     // const printData = () => { 
     //     // console.log("skriv ut i konsollen");
@@ -70,33 +121,6 @@ const Editor = () => {
     //     };
     // };
 
-    let handleSelectedDoc = () => {
-        // Hämta värde ur selectlista
-        let selectedDocId = document.getElementById("selectDoc").value;
-        let selectedDoc = docs[selectedDocId];
-
-        setCurrentDoc(selectedDoc);
-        if (selectedDoc !== undefined) {
-            setEditorContent(selectedDoc);
-        }
-     };
-
-    // console.log(event); // hela html
-    // console.log(event.target); // De använde event.target.value
-    // function setEditorContent(content, triggerChange) {
-    function setEditorContent(doc) {
-        // let element = document.getElementById("selectDoc"); // element.editor is undefined - ej med nedanstående element som är samma sak
-        let element = document.querySelector("trix-editor");
-
-        element.value = "";
-        // element.editor.setSelectedRange([0, 0]);
-        element.editor.insertHTML(doc.html);
-};
-
-    const resetDb = async () => {
-        await docsModel.reset();
-    };
-
     const setDocName = (event) => {
         // console.log(event.target.value);
         setName(event.target.value);
@@ -105,21 +129,13 @@ const Editor = () => {
     const showSaveForm = () => {
         // Visa formuläret med CSS
         document.getElementById("saveForm").style.display = "block";
-
     };
 
     const createObject = async (event) => {
         event.preventDefault();
         let newDoc = {};
         newDoc.html = data;
-        // console.log("Hämta namn");
-        // let docName = document.getElementById("saveForm").value;
-        // let docName = document.querySelector("saveForm").value; // document.querySelector(...) is null
-        // console.log(docName); //undefined
         newDoc.name = name;
-
-        // console.log("emit newDoc");
-        // socket.emit("createDoc", newDoc); // Skapar ett room på servern med
         await docsModel.create(newDoc);
     };
 
@@ -127,42 +143,11 @@ const Editor = () => {
         if (currentDoc === undefined) {
             alert("Please choose a file to update");
         } else {
-            currentDoc.html = data; // Ändrar html för currentDoc till det som står i editorn
+            console.log(currentDoc);
+            // console.log(data);
+            // currentDoc.html = data; // Ändrar html för currentDoc till det som står i editorn
             await docsModel.update(currentDoc);
-            // console.log("current_doc_id ", currentDoc._id);
-            // console.log("current_data ", data);
         }
-    };
-
-    // const saveObject = () => { 
-    //     console.log( "spara objekt" );
-    // };
-
-    // let handleEditor = (e, editor) => { 
-    //     // setData(editor.getData());
-    //     setData("nytt");
-    // }
-
-    // handleEditorReady(editor) {
-    //     // this is a reference back to the editor if you want to
-    //     // do editing programatically
-    //     editor.insertString("editor is ready");
-    //   };
-
-    let setEditorData = (event) => {
-
-
-        setData(event);
-        currentDoc.html = data;
-        console.log(data);
-
-        console.log("currentDoc", currentDoc);
-        // console.log(docs[selectedDocId]);
-        // console.log("emit create");
-        // socket.emit("create", docs[selectedDocId]);
-        socket.emit("create", currentDoc);
-
-
     };
 
     return (
@@ -188,11 +173,8 @@ const Editor = () => {
                 <input className = "button" type = "submit" value = "Save"></input>
             </form>
 
-
             <TrixEditor id = "trixEditorContent" className = "trix-editor" toolbar = "trix-toolbar"
-                // editor = {TrixEditor}
-                // onEditorReady = {(event, editor) => {onEditorReady(event, editor) }}
-                onChange = { setEditorData } 
+                onChange = { handleChange }
                 // onChange={props.change}
                 // value = { data }
                 // input = 'react-trix-editor'
@@ -200,22 +182,18 @@ const Editor = () => {
                 // default={props.default}
             />
         </div>
-
-
     )
 }
 
 export default Editor
 
 
-// Exemplet från föreläsning med att socket in ska trigga ett change-event i frontend:
+// Exemplet från föreläsning med att socket inte ska trigga ett change-event i frontend:
 
 //   function handleChange(html, text) {
 //     if (updateCurrentDocOnChange) {
 //         const copy = Object.assign({}, currentDoc);
-
 //         copy.html = html;
-
 //         setCurrentDoc(copy);
 //     }
 
@@ -256,8 +234,6 @@ export default Editor
 
 // const cursorPos = useRef([]);
 
- 
-
 // function setEditorContent(content: string, triggerChange: boolean) {
 //         let element = document.querySelector("trix-editor") as any | null;
 
@@ -271,17 +247,3 @@ export default Editor
 //         // Set selected range to the "old" cursor position
 //         element.editor.setSelectedRange(cursorPos.current);
 //     }
-
-
-// eriknastesjo
-//  — 
-// Idag 09:23
-// Hej! Jag kanske missade när du använde setEditorContent i tidigare kmom. När/hur ska man kunna hinna kalla på den för att ändra updateCurrentDocOnChange till false innan onChange-eventet sker? Tänker just i samband med sockets.
-// efo
-//  — 
-// Idag 09:57
-// I min kod ser det ut så här:
-
-// socket.on("docsData", (data) => {
-//       setEditorContent(data.html, false);
-//     });
